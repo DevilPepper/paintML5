9fa01c62f10cce2f2d0f17f8a31a1ac1c2571868
SupaStuff <StuffQilla@gmail.com>
2016-03-29 15:29:12 -0400

    paint buckett attempt #2

--- js/paintStuff.js
+++ js/paintStuff.js
@@ -3,13 +3,14 @@ var brush = "brush";
 var line = "line";
 var rectangle = "rectangle";
 var eraser = "eraser";
+var bucket = "bucket"
 
 //record keepers
 var numsteps = 0;
 var steps = [];
 
 //current stuff
-var currColor = "#000000";
+var currColor;
 var currSize = 5;
 var currTool = brush;
 var mouseisdown = false;
@@ -30,6 +31,7 @@ var Tool = function (size, color, tool, start){
 	this.points = [start];
 	//start the shape with start and end points at the start.
 	if(tool === rectangle || tool === line) this.points.push(start);
+	else if(tool === bucket) this.points.pop();
 };
 Tool.prototype.constructor = Tool;
 //add points to the stroke to make it longer
@@ -55,10 +57,98 @@ function getMousePos(e, that)
 	return {x: loc.x - that.offsetLeft, y: loc.y - that.offsetTop};
 }
 
+function rgbToHex(r, g, b)
+{
+	var hexed = "000000" + ((r*256*256) + (g*256) + b).toString(16);
+	return hexed.substr(hexed.length - 6);
+}
+
+function getPixel(coords)
+{
+	return coords.y * context.canvas.width + coords.x;
+}
+
+function getColor(coords)
+{
+	//var imageData = context.getImageData(coords.x, coords.y, 1, 1);
+	//var pixel = coords.y * context.canvas.width + coords.x;
+	//var red = imageData.data[pixel];
+	//var green = imageData.data[pixel+1];
+	//var blue = imageData.data[pixel+2];
+	//var alpha = imageData.data[pixel+3];
+	var cl = context.getImageData(coords.x, coords.y, 1, 1).data;
+	//console.log(cl);
+	return ("#" + rgbToHex(cl[0], cl[1], cl[2]));
+}
+
+function getColor2(r, g, b)
+{
+	return ("#" + rgbToHex(r, g, b));
+}
+
+function pbucket(coords, targetCl, startup)
+{
+	if(targetCl != currColor)
+	{
+		var imageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
+		var Q = [];
+		var processed = new Array(context.canvas.width * context.canvas.height).fill(false);
+		var currPixel;
+		var pixel;
+		var up, down, left, right;
+		Q.push(coords);
+		processed[getPixel(coords)] = true;
+		while(Q.length > 0)
+		{
+			currPixel = Q.shift(); //pop_front()
+			console.log(currPixel);
+			pixel = getPixel(currPixel);
+			if(getColor2(imageData.data[pixel], imageData.data[pixel + 1], imageData.data[pixel + 2]) == targetCl)
+			{
+				if(startup != true) steps[numsteps-1].points.push(coords);
+				context.fillRect(coords.x, coords.y, 1, 1);
+				up = {x:currPixel.x, y:currPixel.y-1};
+				down = {x:currPixel.x, y:currPixel.y+1};
+				left = {x:currPixel.x-1, y:currPixel.y};
+				right = {x:currPixel.x+1, y:currPixel.y};
+
+				if(!processed[getPixel(right)] && currPixel.x < context.canvas.width) Q.push(right);
+				if(!processed[getPixel(left)] && currPixel.x > 0) Q.push(left);
+				if(!processed[getPixel(down)] && currPixel.y < context.canvas.height) Q.push(down);
+				if(!processed[getPixel(up)] && currPixel.y > 0) Q.push(up);
+
+				processed[getPixel(up)] = true;
+				processed[getPixel(down)] = true;
+				processed[getPixel(left)] = true;
+				processed[getPixel(right)] = true;
+			}
+		}
+	}
+}
+
+/*
+function pbucket(coords, color)
+{
+	var cl = getColor(coords);
+	if(cl === color && color != currColor)
+	{
+		steps[numsteps-1].points.push(coords);
+		context.fillRect(coords.x, coords.y, 1, 1);
+		if(coords.x > 0)pbucket({x:coords.x - 1,y:coords.y},color);
+		if(coords.x < context.canvas.width)pbucket({x:coords.x + 1,y:coords.y},color);
+		if(coords.y > 0)pbucket({x:coords.x,y:coords.y - 1},color);
+		if(coords.y < context.canvas.height)pbucket({x:coords.x,y:coords.y + 1},color);
+	}
+}
+*/
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 $(document).ready(function(){
 	context = document.getElementById('paintML5').getContext("2d");
+	//currColor = "#FFFFFF";
+	//pbucket({x:0, y:0}, "000000", true);
+	//currColor = "#000000"
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////
@@ -116,7 +206,15 @@ $('#paintML5').on('mousedown touchstart', function(e){
 	//add this step to the array
 	steps.push(new Tool(currSize, currColor, currTool, mouse));
 	mouseisdown = true;
+	
 	redraw();
+	
+	if(steps[numsteps-1].tool == bucket)
+	{
+		context.fillStyle = currColor;
+		console.log(getColor(mouse));
+		pbucket(mouse, getColor(mouse));
+	}
 });
 
 $('#paintML5').on('mouseup mouseleave touchend', function(e){
@@ -138,6 +236,7 @@ function redraw(){
 	for(var i=0; i < numsteps; i++){
 		//set the color and width of this path
 		context.strokeStyle = steps[i].color;
+		context.fillStyle = steps[i].color;
 		context.lineWidth = steps[i].size;
 		
 		//if this is a rectangle use context.rect to draw it
@@ -149,6 +248,11 @@ function redraw(){
 			context.rect(coordStart.x, coordStart.y, (coordEnd.x-coordStart.x), (coordEnd.y-coordStart.y));
 			context.closePath();
 			context.stroke();
+		}
+		else if(steps[i].tool == bucket)
+		{
+			for(var j=0; j<steps[i].points.length; j++)
+					context.fillRect(steps[i].points[j].x, steps[i].points[j].y, 1, 1);
 		}
 		//otherwise, draw all of the \delta lines
 		else
